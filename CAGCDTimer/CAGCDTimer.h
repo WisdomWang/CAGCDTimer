347d808609400003c5e8da6e16773babf1de941f
umer <umer@umerdeMacBook-Pro.local>
2020-03-25 15:20:10 +0800

    first commit

---
+++ CAGCDTimer/CAGCDTimer.h
@@ -0,0 +1,17 @@
+//
+//  CAGCDTimer.h
+//  CAGCDTimer
+//
+//  Created by umer on 2020/3/24.
+//  Copyright © 2020 umer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CAGCDTimer : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CAGCDTimer/CAGCDTimer.m
@@ -0,0 +1,13 @@
+//
+//  CAGCDTimer.m
+//  CAGCDTimer
+//
+//  Created by umer on 2020/3/24.
+//  Copyright © 2020 umer. All rights reserved.
+//
+
+#import "CAGCDTimer.h"
+
+@implementation CAGCDTimer
+
+@end
